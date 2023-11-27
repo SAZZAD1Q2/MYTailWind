2a6fe139bab5d5f62ad37ee4602681b72e4dac6f
SAZZAD1Q2 <kurulia115@gmail.com>
2023-11-27 21:40:53 +0600

    Add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl sm:text-6xl hover:bg-green-500 text-red-500 font-bold p-10 border m-5 text-center border-blue-500">Hello Tailwind</h1>
+      
+    </>
+  )
+}
+
+export default App
